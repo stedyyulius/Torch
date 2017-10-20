c6d0e97679da47a356f03e599466d9b1f10278b9
pdvega <pdvegaa@gmail.com>
2017-10-21 00:28:10 +0700

    room panel

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Dashboard extends Component {
   render(){
     return(
-      <div className="container">
-          
+      <div className="panel panel-default">
+        <div className="panel-heading"></div>
+        <div className="panel-body">Room Panel</div>
       </div>
     )
   }
