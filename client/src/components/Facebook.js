673a17d72c4377101850572433048b35f87af693
stedyyulius <stedyyulius@gmail.com>
2017-10-21 09:59:03 +0700

    login facebook page

--- client/src/components/Facebook.js
+++ client/src/components/Facebook.js
@@ -11,13 +11,29 @@ class Facebook extends Component{
    }
   render(){
     return(
-      <FacebookLogin
-         appId="244681202728125"
-         autoLoad={true}
-         fields="name,email,picture"
-         scope="public_profile,user_friends,user_actions.books"
-         callback={this.responseFacebook}
-       />
+      <div class="body">
+    <div class="login-form">
+      <div class="form-group logo">
+          {/* <img width="150px" src="../assets/logo.png" /> */}
+      </div>
+       <div class="input form-group">
+         <input type="text" class="form-control" placeholder="Username " id="UserName" />
+         <i class="fa fa-user"></i>
+       </div>
+       <div class="form-group log-status">
+         <input type="password" class="form-control" placeholder="Password" id="Passwod" />
+         <i class="fa fa-lock"></i>
+       </div>
+       <FacebookLogin
+          appId="244681202728125"
+          autoLoad={true}
+          fields="name,email,picture"
+          scope="public_profile,user_friends,user_actions.books"
+          callback={this.responseFacebook}
+        />
+       <button type="button" class="log-btn">Log in</button>
+     </div>
+   </div>
     )
   }
 }
